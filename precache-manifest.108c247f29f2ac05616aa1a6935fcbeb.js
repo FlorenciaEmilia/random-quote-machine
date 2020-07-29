2270a999daa40de965df2645fbe6ecf71d4da5ac
Florencia <flxrenciaemilia@gmail.com>
2020-07-29 14:39:03 -0500

    Updates

---
+++ precache-manifest.108c247f29f2ac05616aa1a6935fcbeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d77ce75cd8c0824ac48c4710bec8a905",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "dde39e972976f2ffb053",
+    "url": "/random-quote-machine/static/css/main.7502ee08.chunk.css"
+  },
+  {
+    "revision": "52832cd4f2d3acd616a0",
+    "url": "/random-quote-machine/static/js/2.c9c66e6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-machine/static/js/2.c9c66e6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dde39e972976f2ffb053",
+    "url": "/random-quote-machine/static/js/main.70fc7c20.chunk.js"
+  },
+  {
+    "revision": "52b3aacfd4b50869dda1",
+    "url": "/random-quote-machine/static/js/runtime-main.116f0ca4.js"
+  }
+]);
